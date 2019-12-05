73bcdaa66c2f608d9e4e1c3fe379fd39d1b3bd60
Raul Muñoz <Raulmunozn@gmail.com>
2019-12-05 09:47:40 -0400

    Update conf.py

--- src/conf.py
+++ src/conf.py
@@ -71,7 +71,7 @@ exclude_patterns = []
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-html_show_sourcelink = False
+html_show_sourcelink = True
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
