2d459d9eabe7e1295c4bb4ed518b5d26019aec57
Raul Muñoz <rmunoz@erpcya.com>
2019-12-13 14:54:33 -0400

    Update conf.py
    Add extension for build doc test

--- src/conf.py
+++ src/conf.py
@@ -41,7 +41,7 @@ html_logo = 'static/ERP_Logo.png'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinx.ext.doctest'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
