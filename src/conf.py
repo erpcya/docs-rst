410bf2c81f7ca8b1c5ed3cd651e59ea8ad12d3cc
TaniaLucena <tania.lucena2603@gmail.com>
2020-09-14 16:49:08 -0400

    prueba

--- src/conf.py
+++ src/conf.py
@@ -130,7 +130,7 @@ html_sidebars = {'**': [
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_staticstatic']
-html_static_path = ['_static']
+html_static_path = ['']
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
