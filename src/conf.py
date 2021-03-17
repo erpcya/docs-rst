89e9cc487cfbdb82d2b086e84cea9cee3b8ef35e
TaniaLucena <tania.lucena2603@gmail.com>
2021-03-17 16:29:47 -0400

    cambios

--- src/conf.py
+++ src/conf.py
@@ -50,8 +50,7 @@ html_additional_pages = {"download": "pages/download.html", "index": "pages/inde
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-latex_show_urls = 'footnote' 
-extensions = ['sphinx.ext.doctest', 'sphinx.ext.intersphinx'
+extensions = ['sphinx.ext.doctest'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -162,9 +161,7 @@ latex_elements = {
 
     # Additional stuff for the LaTeX preamble.
     #
-    'preamble': '\\def\\thempfootnote{\\arabic{mpfootnote}}',
-
-    'packages': r'\usepackage{enumitem}'
+    # 'preamble': '',
 
     # Latex figure (float) alignment
     #
