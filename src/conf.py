ccc3ff366f7a75010a3782868dcda631277130b0
TaniaLucena <tania.lucena2603@gmail.com>
2021-03-17 12:24:30 -0400

    prueba

--- src/conf.py
+++ src/conf.py
@@ -151,16 +151,15 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
+
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
+
     # Additional stuff for the LaTeX preamble.
     #
-    'preamble':
-    """\\def\\thempfootnote{\\arabic{mpfootnote}}
-    """,
-    'packages': r'\usepackage{enumitem}'
     # 'preamble': '',
+
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
