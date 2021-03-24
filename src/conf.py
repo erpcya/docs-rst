3bfdadb024a399c18932f2e077da57af58f9e40a
TaniaLucena <tania.lucena2603@gmail.com>
2021-03-24 12:23:00 -0400

    cambios

--- src/conf.py
+++ src/conf.py
@@ -29,7 +29,7 @@ author = 'E.R.P. Consultores y Asociados, C.A.'
 version = '1.0'
 
 # The full version, including alpha/beta/rc tags.
-release = '3.9.3'
+release = '3.9.3 Mini-ERP'
 
 # Html logo in drawer.
 # Fit in the drawer at the width of image is 240 px.
